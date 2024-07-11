3b9b36c175caff210a0dfca94fdb2b5c73940e3e
looper007 <44048399+looper007@users.noreply.github.com>
2024-07-11 20:25:35 +1000

    modified port

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const moviesRouter = require("./routes/movies");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 app.use(cors());
 app.use(express.json());
